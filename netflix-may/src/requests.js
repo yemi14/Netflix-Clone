c6477a5d389081e9c7896f0fa6ccbf490921b371
yeminegash <89469729+yemi14@users.noreply.github.com>
2021-12-01 12:04:09 -0500

    first com

---
+++ netflix-may/src/requests.js
@@ -0,0 +1,11 @@
+const API_KEY = "4168fc9a27f01723c5e541931deffd34";
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  };
+export default requests;
